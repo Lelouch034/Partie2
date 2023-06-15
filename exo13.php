5a21c784895387d474ff336870cc8aa8fb90a71e
Lelouch034 <e.mignot6700@gmail.com>
2023-06-15 15:37:08 +0200

    oui

---
+++ exo13.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo13</title>
+</head>
+<body>
+    <h1>Exercice 13 </h1>
+    <p>Créer une classe Voiture possédant les propriétés suivantes : marque, modèle, nbPortes et
+vitesseActuelle ainsi que les méthodes demarrer( ), accelerer( ) et stopper( ) en plus
+des accesseurs (get) et mutateurs (set) traditionnels. La vitesse initiale de chaque véhicule
+instancié est de 0. Une méthode personnalisée pourra afficher toutes les informations d’un
+véhicule.</p>
+    <h2></h2>
+</body>
+</html>
+
+
+<!-- ***********php******* -->
+<?php
+
+// Création de ma class 
+//Properties
+class Personne {
+    private string $marque;
+    private string $modéle;
+    private int $nbPortes;
+    private int $vitesseActuelle;
+    
+    //Methods 
+
+
+
+}
+
+
+
+
+?>
