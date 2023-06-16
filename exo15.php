f55b32c3ee2f714da68c38c882481385f6bf0585
Lelouch034 <e.mignot6700@gmail.com>
2023-06-16 14:56:25 +0200

    oui

---
+++ exo15.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo1</title>
+</head>
+<body>
+    <h1>Exercice 15 </h1>
+    <p>En utilisant les ressources de la page http://php.net/manual/fr/book.filter.php, vérifier si une
+adresse e-mail a le bon format.</p>
+    <h2></h2>
+</body>
+</html>
+
+
+<!-- ***********php******* -->
+<?php
+
+
+?>
