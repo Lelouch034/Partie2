6c20b3734e2a26b6ff18406944deda1c67f7a2c8
Lelouch034 <e.mignot6700@gmail.com>
2023-06-16 15:28:02 +0200

    fth

--- exo15.php
+++ exo15.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>exo1</title>
+    <title>exo15</title>
 </head>
 <body>
     <h1>Exercice 15 </h1>
